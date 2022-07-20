050e7c7ab1510ebee0e9092192d98dc1abe9cc31
andmatcha <jj.mcr185@gmail.com>
2022-07-20 21:56:56 +0900

    initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace HelloWorld;
+
+class Hello {
+    static string greeting = "Hello, world!";
+
+    static void Main()
+    {
+        Console.WriteLine(greeting);
+        Console.Write("Enter your name >");
+        string? name = Console.ReadLine();
+        Console.WriteLine($"Hello, {name}!");
+    }
+}
